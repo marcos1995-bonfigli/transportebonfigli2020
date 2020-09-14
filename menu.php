65e5fcb77ccf0fd5fed8002dc5286482d58a1dcc
nicocoronel <nicocoro15@gmail.com>
2020-09-14 15:59:08 -0300

    Update menu.php

---
+++ menu.php
@@ -0,0 +1,36 @@
+<?php session_start();
+
+if (isset($_SESSION['usuario'])) {
+
+}else{
+	header('Location: login.php');
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<link href="https://fonts.googleapis.com/css2?family=Raleway:wght@300;400&display=swap" rel="stylesheet">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+	<link rel="stylesheet" href="assets/css/menu.css">
+	<title>Contenido</title>
+</head>
+<body>
+	<div class="contenedor">
+		<h1 class="titulo">Contenido del sitio</h1>
+		<a href="cerrar.php">Cerrar Sesion</a>
+		<hr class="border">
+
+		<div class="contenido">
+			<article>
+				<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+
+				<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+			</article>
+		</div>
+	</div>
+</body>
+</html>
