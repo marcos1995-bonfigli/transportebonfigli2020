cc282fbb44934f1fc0f9fd2f21ef75c4a03147d6
tomig7 <tomigonzalez99@hotmail.com>
2020-09-29 17:26:30 -0300

    Update adminstrador.php

---
+++ adminstrador.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+
+<html lang="es">
+
+<header>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/administrador.css">
+
+ </header>
+
+ <body>
+
+ <div class ="contenedorMenu">
+
+        <div>
+            Menú Principal
+        </div>
+
+        <div>
+            Opcion 2
+        </div>
+
+        <div>
+            Opcion 2
+        </div>
+
+        <div>
+            Configuración
+        </div>
+
+ </div>
+
+
+
+
+
+
+
+
+
+     
+ </body>
+
+</html>
