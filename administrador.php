7ab9601d5a8893baa012da4cc7ef3dc9ec869cf6
tomig7 <tomigonzalez99@hotmail.com>
2020-09-29 17:56:08 -0300

    Create administrador.php

---
+++ administrador.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+
+<html lang="es">
+
+<header>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/administrador.css">
+
+ </header>
+
+ <body>
+
+ <div class ="contenedorMenu">
+
+       
+    <ul >
+        <li>
+        <a href="#">Menú Principaaaal</a>
+        </li>
+        <li>
+        <a href="#">Opcion 2</a>
+        </li>
+
+        <li>
+        <a href="#">Opcion 3</a>
+        </li>
+
+        <li>
+        <a href="#">Configuracion</a>
+        </li>
+    </ul>
+
+ </div>
+
+
+
+
+
+
+
+
+
+     
+ </body>
+
+</html>
