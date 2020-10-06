32e2a9835fecf4c845a4978c576af0226d4ec5d4
tomig7 <tomigonzalez99@hotmail.com>
2020-10-05 21:36:12 -0300

    Update menu.php

--- menu.php
+++ menu.php
@@ -8,45 +8,81 @@ if (isset($_SESSION['usuario'])) {
 
 ?>
 
-<!DOCTYPE html>
+<!DOCTYPE html5!>
+
 <html lang="en">
 <head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-	<link href="https://fonts.googleapis.com/css2?family=Slabo+27px&display=swap" rel="stylesheet">
-	<script src="https://kit.fontawesome.com/d73086788b.js" crossorigin="anonymous"></script>
-	<link rel="stylesheet" href="assets/css/menu.css">
-	<title>Principal | Transporte Bonfigli SRL</title>
+
+    
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <!-- Conexion CSS -->
+      <link rel="stylesheet" href="assets/css/menu.css">
+
+    <!-- Iconos   -->
+      <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+
+      <title>Administrador - Transporte Bonfigli</title>
+
 </head>
-<body>
-	<header>
-		<div class="contenedor">
-			<div class="left">
-				<a href="http://www.transportebonfigli.com.ar/index.html" target="_blank"><img src="assets/images/logo.png" id="logo" title="Transporte Bonfigili SRL"></a>
-			</div>
 
-			<div class="medium">
-				<h3>Panel Principal</h3>
-			</div>
+ <body>
 
-			<div class="right">
-				<ul>
-					<li><i class="far fa-bell" style="font-size:20px; color:#B8711B"></i></li>
-					<li><i class="far fa-question-circle" style="font-size:20px; color:#B8711B"></i></li>
-					<li><?php echo $_SESSION['usuario']; ?> <i class="far fa-user" style="font-size:20px; color:#B8711B"></i></li>
-					<li><a href="logout.php">Cerrar Sesion</a></li>
-				</ul>
-			</div>
-		</div>
-	</header>
+    <header>
 
-	<nav class ="contenedorMenu">
-	    <ul>
-	        <a href="#"><li><i class="fas fa-home"></i><br>Menú Principal</li></a>
-	        <a href="#"><li><i class="fas fa-truck"></i><br>Gestor de Choferes</li></a>
-	        <a href="#"><li><i class="fas fa-paper-plane"></i><br>Registro de Actividades</li></a>
-	        <a href="#"><li><i class="fas fa-cog"></i><br>Configuración</li></a>
-	    </ul>
-	</nav>
-</body>
+      <nav class="infoUsuario">
+
+        <ul>
+
+          <li><a href="#"><i class="far fa-user"></i> <?php echo $_SESSION['usuario']; ?></a>
+        <ul>
+          <li><a href="#"><i class="far fa-question-circle"></i> Ayuda</a></li>
+         <li><a href="logout.php"><i class="far fa-times-circle"></i> Cerrar Sesión</a></li>
+         </ul>
+  
+        </ul>
+        
+    </nav>
+
+    
+
+  
+      <img src="assets/images/logo.png" alt="">
+
+     
+      
+
+
+    </header>
+
+   
+
+ <nav class ="contenedorMenu">
+
+
+     <ul>
+       
+
+        <a href="#"><li><i class="fas fa-home"></i><br>Menú Principal</li></a>
+
+        <a href="#"><li><i class="fas fa-truck"></i><br>Gestor de Choferes</li></a>
+
+        <a href="#"><li><i class="fas fa-paper-plane"></i><br>Registro de Actividades</li></a>
+
+        <a href="#"><li><i class="fas fa-tools"></i><br>Configuración</li></a>
+    </ul>
+
+</nav>
+
+
+
+
+
+
+
+
+
+     
+ </body>
+
 </html>
