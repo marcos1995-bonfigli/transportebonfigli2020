b0ff7380543690343c8ba7fde68954218ea1f59b
tomig7 <tomigonzalez99@hotmail.com>
2020-10-06 22:34:53 -0300

    Update menu.php

--- menu.php
+++ menu.php
@@ -17,10 +17,23 @@ if (isset($_SESSION['usuario'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
       <!-- Conexion CSS -->
-      <link rel="stylesheet" href="assets/css/menu.css">
+      <link rel="stylesheet" href="menu.css">
+      
 
     <!-- Iconos   -->
       <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+
+
+       <!-- jQuery y JS   -->
+
+       <script src=".vscode/jquery-3.5.1.min.js"></script>
+       <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+       <script type="text/javascript" src="js.js"></script>
+
+
+
+
+     
 
       <title>Administrador - Transporte Bonfigli</title>
 
@@ -44,13 +57,8 @@ if (isset($_SESSION['usuario'])) {
         
     </nav>
 
-    
 
-  
-      <img src="assets/images/logo.png" alt="">
-
-     
-      
+      <img src="images/logo.png" alt="">
 
 
     </header>
@@ -63,24 +71,20 @@ if (isset($_SESSION['usuario'])) {
      <ul>
        
 
-        <a href="#"><li><i class="fas fa-home"></i><br>Menú Principal</li></a>
+      <li><a  href="#"><i class="fas fa-home"></i><br>Menú Principal</li></a>
 
-        <a href="#"><li><i class="fas fa-truck"></i><br>Gestor de Choferes</li></a>
+      <li><a href="#"><i class="fas fa-users"></i><br>Gestor de Choferes</li></a>
 
-        <a href="#"><li><i class="fas fa-paper-plane"></i><br>Registro de Actividades</li></a>
+      <li><a href="#"><i class="fas fa-truck"></i><br>Gestor de Camiones</li></a>
 
-        <a href="#"><li><i class="fas fa-tools"></i><br>Configuración</li></a>
+      <li><a href="#"><i class="far fa-clipboard"></i><br>Hojas de ruta</li></a>
+
+      <li><a href="#"><i class="fas fa-envelope-open-text"></i></i><br>Enviar E-Mail</li></a>
+
+      <li> <a href="#"><i class="fas fa-tools"></i><br>Configuración</li></a>
     </ul>
 
 </nav>
-
-
-
-
-
-
-
-
 
      
  </body>
