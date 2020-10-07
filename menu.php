dbf1a59f705557dda09cc0ba847e93348c7b5dcc
nicocoronel <nicocoro15@gmail.com>
2020-10-07 19:58:01 -0300

    Update menu.php

--- menu.php
+++ menu.php
@@ -1,90 +1,33 @@
 <?php session_start();
 
-if (isset($_SESSION['usuario'])) {
-	'menu.php';
-}else{
-	header('Location: login.php');
-}
-
 ?>
 
-<!DOCTYPE html5!>
-
+<!DOCTYPE html>
 <html lang="en">
 <head>
-
-    
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <!-- Conexion CSS -->
-      <link rel="stylesheet" href="menu.css">
-      
-
-    <!-- Iconos   -->
-      <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
-
-
-       <!-- jQuery y JS   -->
-
-       <script src=".vscode/jquery-3.5.1.min.js"></script>
-       <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-       <script type="text/javascript" src="js.js"></script>
-
-
-
-
-     
-
-      <title>Administrador - Transporte Bonfigli</title>
-
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<link href="https://fonts.googleapis.com/css2?family=Slabo+27px&display=swap" rel="stylesheet">
+	<script src="https://kit.fontawesome.com/d73086788b.js" crossorigin="anonymous"></script>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+    <script type="text/javascript" src="assets/js/menu.js"></script>
+	<link rel="stylesheet" href="assets/css/menu.css">
 </head>
-
- <body>
-
-    <header>
-
-      <nav class="infoUsuario">
-
+<body>
+    <nav class ="contenedorMenu">
         <ul>
-
-          <li><a href="#"><i class="far fa-user"></i> <?php echo $_SESSION['usuario']; ?></a>
-        <ul>
-          <li><a href="#"><i class="far fa-question-circle"></i> Ayuda</a></li>
-         <li><a href="logout.php"><i class="far fa-times-circle"></i> Cerrar Sesión</a></li>
-         </ul>
-  
+            <a href="dashboard.php"><li><i class="fas fa-home"></i><br>Dashboard</li></a>
+            <a href="choferes.php"><li><i class="fas fa-users"></i><br>Gestor de Choferes</li></a>
+            <a href="#"><li><i class="fas fa-truck"></i><br>Gestor de Camiones</li></a>
+            <a href="#"><li><i class="far fa-clipboard"></i><br>Hojas de Ruta</li></a>
+            <a href="#"><li><i class="fas fa-envelope-open-text"></i><br>Enviar E-Mail</li></a>
+            <a href="#"><li><i class="fas fa-cog"></i><br>Configuración</li></a>
         </ul>
-        
     </nav>
-
-
-      <img src="images/logo.png" alt="">
-
-
-    </header>
-
-   
-
- <nav class ="contenedorMenu">
-
-
-     <ul>
-       
-
-      <li><a  href="#"><i class="fas fa-home"></i><br>Menú Principal</li></a>
-
-      <li><a href="#"><i class="fas fa-users"></i><br>Gestor de Choferes</li></a>
-
-      <li><a href="#"><i class="fas fa-truck"></i><br>Gestor de Camiones</li></a>
-
-      <li><a href="#"><i class="far fa-clipboard"></i><br>Hojas de ruta</li></a>
-
-      <li><a href="#"><i class="fas fa-envelope-open-text"></i></i><br>Enviar E-Mail</li></a>
-
-      <li> <a href="#"><i class="fas fa-tools"></i><br>Configuración</li></a>
-    </ul>
-
-</nav>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
 
      
  </body>
